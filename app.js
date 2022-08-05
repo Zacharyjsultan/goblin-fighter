e2509c56fe59de0521c02738cba698675709682f
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-08-05 12:50:50 -0700

    time for fun stuff

--- app.js
+++ app.js
@@ -2,24 +2,23 @@
 
 const orcListEl = document.querySelector('.orcs-list');
 
-console.log(orcListEl);
 
 // let state
   // user sees orc from on load
 let orcs = [
     { name: 'Miesenplauz', hp: 3, age:.3 },
-    { name: 'Fausto', hp: 1, age: 902 },
-    { name: 'Kauwuhbuhngah', hp:10, age:32 },
+    { name: 'Kauwuhbuhngmeh..', hp: 1, age: 902 },
+    { name: 'Fausto', hp:10, age:32 },
+    { name: 'Pestorugalis', hp:6, age:13 },
 ];
-
+   // loop for rendering of da orc men
 for (let orc of orcs) {
     const orcEl = document.createElement ('div');
-    // create dom element for orc and set content
-    orcEl.textContent = `${orc.name} 👹 (${orc.age} Eons)(${orc.hp} HP)`;
-   // add orcs to div element
+    // create html element for orc and set content for each orc
+    orcEl.textContent = `Orc ${orc.name} - 👹 - ${orc.age} Eons Old - ${orc.hp} HP`;
+   // add orcs to div element for css purposes
     orcEl.classList.add('orc');
     
-    console.log(orcEl);
     //append list of orcs to the div
 
     orcListEl.append(orcEl);
