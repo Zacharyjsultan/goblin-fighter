bd5be42dbb20a156b64219077ed77a6b1965383c
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-08-05 11:08:39 -0700

    orcs are here!

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 // import functions and grab DOM elements
 
+const orcListEl = document.querySelector('.orcs-list');
+
+console.log(orcListEl);
+
 // let state
-  // user sees goblins from on load
+  // user sees orc from on load
+let orcs = [
+    { name: 'Miesenplauz', hp: 3, age:.3 },
+    { name: 'Fausto', hp: 1, age: 902 },
+    { name: 'Kauwuhbuhngah', hp:10, age:32 },
+];
 
+for (let orc of orcs) {
+    const orcEl = document.createElement ('div');
+    // create dom element for orc and set content
+    orcEl.textContent = `${orc.name} 👹 (${orc.age} Eons)(${orc.hp} HP)`;
+   // add orcs to div element
+    orcEl.classList.add('orc');
+    
+    console.log(orcEl);
+    //append list of orcs to the div
 
+    orcListEl.append(orcEl);
+}
 // set event listeners 
   // get user input
   // use user input to update state 
