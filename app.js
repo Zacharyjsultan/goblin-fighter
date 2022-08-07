6cc134553b1129e39c48b2cb0d0cfbc77abe8b3d
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-08-07 10:21:01 -0700

    sunday reassess

--- app.js
+++ app.js
@@ -2,45 +2,48 @@
 const addOrcButton = document.getElementById('add-orc-button');
 const OrcInputEl = document.querySelector('.orc-input');
 
+const killedOrcs = document.querySelector('dead-orcs');
+
+const heroHp = document.querySelector('hero-hp');
+const heroEmoji = document.querySelector('hero-emoji');
+
 const orcListEl = document.querySelector('.orcs-list');
 
+let heroHp = 10;
+let killedOrcs = 0;
 
-// let state
-  // user sees orc from on load
 let orcs = [
     { name: 'Miesenplauz', hp: 3, age:.3 },
     { name: 'Kauwuhbuhngmeh..', hp: 1, age: 902 },
     { name: 'Fausto', hp:10, age:32 },
     { name: 'Pestorugalis', hp:6, age:13 },
 ];
-   // loop for rendering of da orc men
-for (let orc of orcs) {
-    const orcEl = document.createElement ('div');
-    // create html element for orc and set content for each orc
-    orcEl.textContent = `Orc ${orc.name} - 👹 - ${orc.age} Eons Old - ${orc.hp} HP`;
-   // add orcs to div element for css purposes
-    orcEl.classList.add('orc');
-    
-    //append list of orcs to the div
+  
 
-    orcListEl.append(orcEl);
-}
-//add a new goblin to the array
+
 addOrcButton.addEventListener('click', () => {
-
-//get the name of the new goblin from the input
     const name = OrcInputEl.value;
-//make a new goblin object
+
     const newOrc = {
-        name: name,
+        name: OrcInputEl.value,
         hp: 3,
         age: 1,
     };
-//push this object into our goblin array state
     orcs.push(newOrc);
-    OrcInputEl.value = '';
 
-  
-//as we loop, examine the HP. if the HP is 0, render a flame emojis. otherwise, render a goblin emoji.
-
+    displayOrcs();
 });
+
+function displayOrcs() {
+  orcListEl.textContent = '';
+
+  for (let orc of orcs) {
+    const orcEl = document.createElement ('div');
+    
+    orcEl.textContent = `Orc ${orc.name} - 👹 - ${orc.age} Eons Old - ${orc.hp} HP`;
+  
+    orcEl.classList.add('orc');
+    orcListEl.append(orcEl);
+}
+
+}
