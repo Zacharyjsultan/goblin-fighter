e5693cb414b2f081865cf25b3c385c0f8e011702
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-08-07 10:43:31 -0700

    reboot

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 // import functions and grab DOM elements
-const addOrcButton = document.getElementById('add-orc-button');
+
 const OrcInputEl = document.querySelector('.orc-input');
-
-const killedOrcs = document.querySelector('dead-orcs');
-
-const heroHp = document.querySelector('hero-hp');
-const heroEmoji = document.querySelector('hero-emoji');
-
+const killedOrcsEl = document.querySelector('dead-orcs');
+const heroHpEl = document.querySelector('.hero-hp');
+const heroEmoji = document.querySelector('.hero-emoji');
 const orcListEl = document.querySelector('.orcs-list');
-
+const form = document.querySelector('form');
+//
 let heroHp = 10;
 let killedOrcs = 0;
-
 let orcs = [
     { name: 'Miesenplauz', hp: 3, age:.3 },
     { name: 'Kauwuhbuhngmeh..', hp: 1, age: 902 },
     { name: 'Fausto', hp:10, age:32 },
     { name: 'Pestorugalis', hp:6, age:13 },
 ];
-  
 
+//
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-addOrcButton.addEventListener('click', () => {
-    const name = OrcInputEl.value;
+    const data = new FormData (form);
+    
+    const orcName = data.get('orc-input');
 
     const newOrc = {
         name: OrcInputEl.value,
@@ -33,17 +33,12 @@ addOrcButton.addEventListener('click', () => {
 
     displayOrcs();
 });
-
+//
 function displayOrcs() {
-  orcListEl.textContent = '';
+    orcListEl.textContent = '';
 
-  for (let orc of orcs) {
-    const orcEl = document.createElement ('div');
-    
-    orcEl.textContent = `Orc ${orc.name} - 👹 - ${orc.age} Eons Old - ${orc.hp} HP`;
-  
-    orcEl.classList.add('orc');
-    orcListEl.append(orcEl);
-}
+    for (let orc of orcs) {
+        const OrcEl = renderOrc(orc);
+    }
 
 }
